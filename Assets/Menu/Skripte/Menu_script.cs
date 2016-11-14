178801c0c67fce5f2e5c56ce22953d7dfffa3bd2
klemenkac <xklemenx@gmail.com>
2016-11-14 20:50:43 +0100

    Menu
    Ustvarjen začetni zaslon in meni za igro. Vsebuje tudi C# skripto, ki
    omogoča preklope na druge scene oz. zaslone.

---
+++ Assets/Menu/Skripte/Menu_script.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Menu_script : MonoBehaviour {
+
+	public void ChangeScene (string sceneName){
+		SceneManager.LoadScene(sceneName);
+	}
+}
