225a93c9a13fbb5b2f087bd90a5405a2d45b5637
cola9 <nikolaj.colic@gmail.com>
2016-11-15 19:34:34 +0100

    Naredil CPP kviz
    V CPP okno sem dodal tekst za besedilo vprašanja in tri gumbe za
    odgovore. Nato sem dodal 5 statičnih vprašanj in napisal random
    funkcijo, ki vedno prikaže eno vprašanje. Vsem trem gumbom sem dodal
    funkcijo, ki preveri ali smo kliknili na pravilen odgovor. Trenutno le
    v konzoli izpiše ali je odgovor pravilen ali pa napačen.

---
+++ Assets/CPP/Skripte/GameManager.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;// za list
+using System.Linq;//za unansweredQuestions = questions.ToList<Question>();
+using UnityEngine.UI;// za text
+using UnityEngine.SceneManagement;//
+public class GameManager : MonoBehaviour {
+	/*
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}*/
+
+	public Question[] questions;
+	private static List<Question> unansweredQuestions;
+
+	private Question currentQuestion;
+
+	[SerializeField]
+	private Text questionText;
+
+	[SerializeField]
+	private Text odgovor1;
+
+	[SerializeField]
+	private Text odgovor2;
+
+	[SerializeField]
+	private Text odgovor3;
+
+	[SerializeField]
+	private float timeBetweenQuestions = 1f;//1 second delay
+
+	void Start(){
+		if (unansweredQuestions == null || unansweredQuestions.Count == 0) {
+			unansweredQuestions = questions.ToList<Question>();
+		}
+		SetCurrentQuestion ();
+		//Debug.Log (currentQuestion.question + " is " + currentQuestion.isTrue); //izpis v konzoli
+	}
+
+
+	void SetCurrentQuestion(){
+		int randomQuestionIndex = Random.Range (0, unansweredQuestions.Count);
+		currentQuestion = unansweredQuestions[randomQuestionIndex];
+
+		questionText.text = currentQuestion.question;
+		odgovor1.text = currentQuestion.odg1;
+		odgovor2.text = currentQuestion.odg2;
+		odgovor3.text = currentQuestion.odg3;
+
+	}
+
+	IEnumerator TransitionToNextQuestion(){
+		unansweredQuestions.Remove(currentQuestion);
+
+		yield return new WaitForSeconds (timeBetweenQuestions);//cakamo med vprasanji
+
+		SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex);
+	}
+	public void UserSelectAnswer1(){
+		if (currentQuestion.pravilniOdg == odgovor1.text) {
+			Debug.Log (currentQuestion.question + " PRAV JE"); //izpis v konzoli
+		} else {
+			Debug.Log (currentQuestion.question + " Narobe JE"); //izpis v konzoli
+		}
+		StartCoroutine (TransitionToNextQuestion ());
+	}
+	public void UserSelectAnswer2(){
+		if (currentQuestion.pravilniOdg == odgovor2.text) {
+			Debug.Log (currentQuestion.question + " PRAV JE"); //izpis v konzoli
+		} else {
+			Debug.Log (currentQuestion.question + " Narobe JE"); //izpis v konzoli
+		}
+		StartCoroutine (TransitionToNextQuestion ());
+	}
+	public void UserSelectAnswer3(){
+		if (currentQuestion.pravilniOdg == odgovor3.text) {
+			Debug.Log (currentQuestion.question + " PRAV JE"); //izpis v konzoli
+		} else {
+			Debug.Log (currentQuestion.question + " Narobe JE"); //izpis v konzoli
+		}
+		StartCoroutine (TransitionToNextQuestion ());
+	}
+}
---
+++ Assets/CPP/Skripte/Question.cs
@@ -0,0 +1,21 @@
+﻿
+[System.Serializable] // to pomeni, da lahko ta razred shranjuj informacije
+public class Question {
+	/*
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}*/
+
+	public string question;
+	public string odg1;
+	public string odg2;
+	public string odg3;
+	public string pravilniOdg;
+	//public bool isTrue;
+}
