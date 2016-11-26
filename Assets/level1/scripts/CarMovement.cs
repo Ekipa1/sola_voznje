e6c3d22828d95e2df96d50f068c6a7bc3f0b0085
ZakoSandi <zakomail10@gmail.com>
2016-11-26 21:32:52 +0100

    popravek
    Popravljena vožnja.

--- Assets/level1/scripts/CarMovement.cs
+++ Assets/level1/scripts/CarMovement.cs
@@ -23,8 +23,8 @@ public class CarMovement : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
-        //position = transform.position;
-      /*  myTrans = transform;
+       /* position = transform.position;
+        myTrans = transform;
         myPos = myTrans.position;
         myRot = myTrans.rotation.eulerAngles;*/
     }
@@ -32,23 +32,25 @@ public class CarMovement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-       // angle = myTrans.eulerAngles.magnitude * Mathf.Deg2Rad;
+        //angle = myTrans.eulerAngles.magnitude * Mathf.Deg2Rad;
+       
 
-        position.x = Mathf.Clamp(position.x, -340f, 340f);
-        position.y = Mathf.Clamp(position.y, -235f, 235f);
-
-         if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.LeftArrow))
          {
-            rb.velocity = new Vector2(-carspeed, 0);
-           // transform.position += transform.forward * Time.deltaTime * carspeed;
-
+           // transform.position += transform.forward * Time.deltaTime * 90.0f;
+           rb.velocity = new Vector2(-carspeed, 0);
+            Naprej();
+            Nazaj();
+            Levo_zavoj();
+            Desno_zavoj();
+            // transform.position += transform.forward * Time.deltaTime * carspeed;
             //myPos.x += Mathf.Cos(angle) * carspeed* Time.deltaTime;
-            //myPos.y += Mathf.Sin(angle) * carspeed * Time.deltaTime;
+            // myPos.y += Mathf.Sin(angle) * carspeed * Time.deltaTime;
             // position.y += Input.GetAxis("Horizontal") * carspeed * Time.deltaTime;
         }
          else
         {
-
+           
         }
         //transform.position = position;
     }
@@ -56,38 +58,58 @@ public class CarMovement : MonoBehaviour {
     public void Levo_zavoj()
     {
         
+         gameObject.transform.Rotate(0,0, 5 * Time.deltaTime * carspeed);
+        //transform.position += transform.forward * Time.deltaTime * 90.0f;
         //rb.velocity = new Vector2(0, carspeed);
         // myRot.z += rotationSpeed;
         // transform.position += Vector3.left * Time.deltaTime * carspeed;
-        transform.Rotate(new Vector3(0.0f, 0.0f, 5.0f) * carspeed * Time.deltaTime);
+        //transform.Rotate(new Vector3(0.0f, 0.0f, 15.0f) * carspeed * Time.deltaTime);
+        // transform.Rotate(0.0f, 0.0f, -90.0f);
     }
 
     public void Desno_zavoj()
     {
+       
+         gameObject.transform.Rotate(0, 0, -5 * Time.deltaTime * carspeed);
         //rb.velocity = new Vector2(0, -carspeed);
         //myRot.z -= rotationSpeed; 
+        // transform.position += transform.forward * Time.deltaTime * 90.0f;
         //transform.position += Vector3.right * Time.deltaTime * carspeed;
-        transform.Rotate(new Vector3(0.0f, 0.0f, -5.0f) * carspeed * Time.deltaTime);
+        //transform.Rotate(new Vector3(0.0f, 0.0f, -15.0f) * carspeed * Time.deltaTime);
+        //transform.Rotate(90.0f, 0.0f, 0.0f);
     }
 
     public void Naprej()
     {
-        rb.velocity = new Vector2(carspeed, 0);
-
-        
+        //transform.position += transform.forward * Time.deltaTime * 90.0f;
+      
+            position.x += carspeed * Time.deltaTime;
+            transform.position += position.x * transform.right;
+      
+        //rb.velocity = new Vector3(carspeed, 0, 0);
+        //rb.velocity = new Vector3(carspeed, 0, 90.0f);
         //transform.position += Vector3.forward * Time.deltaTime * carspeed;
         //rb.velocity = new Vector2(carspeed, 0);
         // myPos.x += (Mathf.Cos(angle) * carspeed) * Time.deltaTime;
         //myPos.y += (Mathf.Sin(angle) * carspeed) * Time.deltaTime;
-        
+
         // transform.Rotate(new Vector3(0.0f, 0.0f, 5.0f) * carspeed * Time.deltaTime);
         //myTrans.position = myPos;
         //myTrans.rotation = Quaternion.Euler(myRot);
+        position.x = Mathf.Clamp(position.x, -445.0f, 445f);
+        position.z = Mathf.Clamp(position.z, -133.0f, 133.0f);
     }
 
     public void Nazaj()
     {
-        rb.velocity = new Vector2(-carspeed, 0);
+        //rb.velocity = new Vector3(-carspeed, 0,0);
+        
+            position.x -= carspeed * Time.deltaTime;
+            transform.position -= position.x * transform.right;
+        
+        // rb.velocity = new Vector3(0, 0, 0);
+        //rb.velocity = new Vector2(-carspeed, 0);
+        // transform.position -= transform.forward * Time.deltaTime * 90.0f;
         //transform.position += Vector3.back * Time.deltaTime * carspeed;
         //rb.velocity = new Vector2(-carspeed, 0);
         //myPos.x += Mathf.Cos(angle) * Time.deltaTime;
@@ -97,10 +119,16 @@ public class CarMovement : MonoBehaviour {
         //transform.Rotate(new Vector3(0.0f, 0.0f, -5.0f) * carspeed * Time.deltaTime);
         //myTrans.position = myPos;
         //myTrans.rotation = Quaternion.Euler(myRot);
+        position.x = Mathf.Clamp(position.x, -445.0f, 445f);
+        position.z = Mathf.Clamp(position.z, -133.0f, 133.0f);
     }
 
     public void Stop()
     {
         rb.velocity = Vector2.zero;
+    }
+
+    public void crash()
+    {
     }
 }
