24877ce3856d7a254bed5d11dc6a71c9cc86446f
cola9 <nikolaj.colic@gmail.com>
2016-11-28 16:24:31 +0100

    update

--- Assets/Menu/Skripte/Menu_script.cs
+++ Assets/Menu/Skripte/Menu_script.cs
@@ -12,7 +12,7 @@ public class Menu_script : MonoBehaviour {
 	public Color btnColor;
 	void Start()
 	{
-		
+
 	}
 	void OnEnable()
 	{
@@ -37,5 +37,11 @@ public class Menu_script : MonoBehaviour {
 	}
 	public void ChangeScene (string sceneName){
 		SceneManager.LoadScene(sceneName);
+	}
+	public void izpis(){
+		StreamWriter writer = new StreamWriter("data.txt");
+		writer.WriteLine("");
+		writer.Close();
+		SceneManager.LoadScene("Login");
 	}
 }
