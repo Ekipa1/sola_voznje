d709fda8f40e1f5aca99ffbe3b2ace54239b30de
ZakoSandi <zakomail10@gmail.com>
2016-11-28 14:09:50 +0100

    popravljena vožnja.
    Popravljena vožnja, sedaj se avtomobil ob držanju buttona za različne
    smeri premika dokler buttona ne spustimo, dodano tudi uničenje
    avtomobila ob zaletu v drug avtomobil.

---
+++ Assets/Button_naprej.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+
+
+public class Button_naprej : MonoBehaviour, IPointerDownHandler  {
+
+    CarMovement cs;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+       // cs.Naprej();
+    }
+}
