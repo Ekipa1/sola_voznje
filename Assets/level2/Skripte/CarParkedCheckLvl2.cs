abf8b872d3f36d75ecab10bd79c41f62f983d6e4
cola9 <nikolaj.colic@gmail.com>
2016-12-01 18:08:34 +0100

    update 2lvl
    Dodal sem da ko uporabnik parkira na potrebno mesto se igra končna in
    izpiše čestitamo. Dodal sem tudi da ko se uporabnik zaleti v drugi
    avto/ograjo se igra resetira.

---
+++ Assets/level2/Skripte/CarParkedCheckLvl2.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class CarParkedCheckLvl2 : MonoBehaviour {
+
+    public GameObject avto;
+	//public Text asd;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		//asd.text="x: " + avto.transform.position.x.ToString () + " y: " + avto.transform.position.y.ToString () + " Z: " + avto.transform.rotation.z.ToString ();
+        //&& avto.transform.position.x<-200 && avto.transform.position.y<203.4 && avto.transform.position.y > 180
+        if (avto.transform.position.x >-137 && avto.transform.position.x < -120 && avto.transform.position.y < 540 && avto.transform.position.y > 525 )
+        {
+            if((avto.transform.rotation.z < -0.85 && avto.transform.rotation.z > -0.95) || (avto.transform.rotation.z > 0.2 && avto.transform.rotation.z < 0.45))
+            {
+                StartCoroutine(MyCoroutine());
+                
+            }
+            //Debug.Log("DELA");
+        }
+       // Debug.Log(avto.transform.rotation.z);
+    }
+
+    IEnumerator MyCoroutine()
+    {
+        //This is a coroutine
+        // SceneManager.LoadScene("Cestitamo");
+
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene("Cestitamo");
+        //DoSomethingElse();
+    }
+}
---
+++ Assets/level2/Skripte/CollisionLvl2.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionLvl2 : MonoBehaviour {
+
+
+    public GameObject avto;
+
+    void Update()
+    {
+       // Debug.Log(avto.transform.rotation.z);
+    }
+
+    
+    // Use this for initialization
+    void OnCollisionEnter2D(Collision2D Collection)
+    {
+        Debug.Log("FEF");
+        if (Collection.gameObject.name == "prvi_avto")
+        {
+            avto.transform.position = new Vector3(-178, 475, 0);
+            avto.transform.Rotate(0, 0, 0);
+            //transform.position = new Vector3(n, transform.position.y, transform.position.z);
+        }
+    }
+}
