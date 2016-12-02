91b3088681141f79652c2b775a382a1274f85940
ZakoSandi <zakomail10@gmail.com>
2016-12-02 02:10:14 +0100

    oblikovan tretji level
    , manjka se samo popravek za park check, trenutno niso pravilne
    kordinate

---
+++ Assets/level3/carparkedchechek3rdlevel/carparkchecked3.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class carparkchecked3 : MonoBehaviour {
+
+    public GameObject avto;
+    //public Text asd;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //asd.text="x: " + avto.transform.position.x.ToString () + " y: " + avto.transform.position.y.ToString () + " Z: " + avto.transform.rotation.z.ToString ();
+        //&& avto.transform.position.x<-200 && avto.transform.position.y<203.4 && avto.transform.position.y > 180
+        if (avto.transform.position.x > -215 && avto.transform.position.x < -190 && avto.transform.position.y < 195 && avto.transform.position.y > 185)
+        {
+            if ((avto.transform.rotation.z < -0.85 && avto.transform.rotation.z > -0.95) || (avto.transform.rotation.z > 0.2 && avto.transform.rotation.z < 0.45))
+            {
+                StartCoroutine(MyCoroutine());
+
+            }
+            //Debug.Log("DELA");
+        }
+        // Debug.Log(avto.transform.rotation.z);
+    }
+
+    IEnumerator MyCoroutine()
+    {
+        //This is a coroutine
+        // SceneManager.LoadScene("Cestitamo");
+
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene("Cestitamo");
+        //DoSomethingElse();
+    }
+}
---
+++ Assets/level3/collisionlvl3/colisionlevel3.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class colisionlevel3 : MonoBehaviour {
+    public GameObject avto;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter2D(Collision2D Collection)
+    {
+        Debug.Log("Crash, you Failed!");
+        if (Collection.gameObject.name == "prvi_avto")
+        {
+            avto.transform.position = new Vector3(-178, 475, 0);
+            avto.transform.localEulerAngles = new Vector3(0, 0, 0);
+            //avto.transform.Rotate(0, 0, 0);
+            //transform.position = new Vector3(n, transform.position.y, transform.position.z);
+        }
+    }
+}
