e4d07b56f8fc9625169de3f101a29ac70a5184b9
ZakoSandi <zakomail10@gmail.com>
2016-12-02 20:30:35 +0100

    lvl3
    level3

--- Assets/Cestitamo/changeScene.cs
+++ Assets/Cestitamo/changeScene.cs
@@ -5,6 +5,14 @@ using UnityEngine.SceneManagement;
 public class changeScene : MonoBehaviour {
 
 	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
 
     public void ChangeScene()
     {
--- Assets/level3/carparkedchechek3rdlevel/carparkchecked3.cs
+++ Assets/level3/carparkedchechek3rdlevel/carparkchecked3.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 public class carparkchecked3 : MonoBehaviour {
 
     public GameObject avto;
+    Rigidbody2D rb;
     //public Text asd;
     // Use this for initialization
     void Start()
@@ -18,9 +19,10 @@ public class carparkchecked3 : MonoBehaviour {
 
         //asd.text="x: " + avto.transform.position.x.ToString () + " y: " + avto.transform.position.y.ToString () + " Z: " + avto.transform.rotation.z.ToString ();
         //&& avto.transform.position.x<-200 && avto.transform.position.y<203.4 && avto.transform.position.y > 180
-        if (avto.transform.position.x > -215 && avto.transform.position.x < -190 && avto.transform.position.y < 195 && avto.transform.position.y > 185)
+
+        if (avto.transform.position.x > -209 && avto.transform.position.x < 140 && avto.transform.position.y < 203 && avto.transform.position.y > 307)
         {
-            if ((avto.transform.rotation.z < -0.85 && avto.transform.rotation.z > -0.95) || (avto.transform.rotation.z > 0.2 && avto.transform.rotation.z < 0.45))
+            if ((avto.transform.rotation.z < 1.4 && avto.transform.rotation.z > -120.1154) || (avto.transform.rotation.z > 0.2 && avto.transform.rotation.z < 0.45))
             {
                 StartCoroutine(MyCoroutine());
 
