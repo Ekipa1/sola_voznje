7dae19c9846fed50f05f29171e4e48250f5ebbb7
cola9 <nikolaj.colic@gmail.com>
2016-12-08 20:03:10 +0100

    Update 3d voznja
    Dodal sem da ko spustimo gas začne avto sam bremzati in se na koncu
    tudi ustavi.

--- Assets/3dVoznja/Scripts/CarControleScript.js
+++ Assets/3dVoznja/Scripts/CarControleScript.js
@@ -10,19 +10,14 @@ var wheelRRTrans : Transform;
 var heighestSpeed : float = 50;//
 var lowSpeedSteerAngel : float = 10;
 var highSpeedSteerAngel : float = 1;
-var maxTorque : float = 100;
+var decelerationSpeed : float = 900;
+var maxTorque : float = 1000;
 function Start () {
 GetComponent.<Rigidbody>().centerOfMass.y=-0.9;
 }
 
 function FixedUpdate () {
-	wheelRR.motorTorque= maxTorque* Input.GetAxis("Vertical");
-	wheelRL.motorTorque= maxTorque* Input.GetAxis("Vertical");
-	var speedFactor = GetComponent.<Rigidbody>().velocity.magnitude/heighestSpeed;
-	var currentSteerAngel = Mathf.Lerp(lowSpeedSteerAngel,highSpeedSteerAngel,speedFactor);
-	currentSteerAngel *= Input.GetAxis("Horizontal");
-	wheelFL.steerAngle = currentSteerAngel;
-	wheelFR.steerAngle = currentSteerAngel;
+	Control();
 }
 
 function Update(){
@@ -32,4 +27,22 @@ function Update(){
 	wheelRRTrans.Rotate(wheelRR.rpm/60*360*Time.deltaTime,0,0);
 	wheelFLTrans.localEulerAngles.y = wheelFL.steerAngle - wheelFLTrans.localEulerAngles.z;
 	wheelFRTrans.localEulerAngles.y = wheelFR.steerAngle - wheelFRTrans.localEulerAngles.z;
+}
+
+function Control() {
+	wheelRR.motorTorque= maxTorque* Input.GetAxis("Vertical");
+	wheelRL.motorTorque= maxTorque* Input.GetAxis("Vertical");
+	if (Input.GetButton("Vertical")==false){//ne drzimo gasa
+		wheelRR.brakeTorque = decelerationSpeed;//brakeTorque avto sam bremza
+		wheelRL.brakeTorque = decelerationSpeed;
+	}
+	else{
+		wheelRR.brakeTorque = 0;
+		wheelRL.brakeTorque = 0;
+	}
+	var speedFactor = GetComponent.<Rigidbody>().velocity.magnitude/heighestSpeed;
+	var currentSteerAngel = Mathf.Lerp(lowSpeedSteerAngel,highSpeedSteerAngel,speedFactor);
+	currentSteerAngel *= Input.GetAxis("Horizontal");
+	wheelFL.steerAngle = currentSteerAngel;
+	wheelFR.steerAngle = currentSteerAngel;
 }
