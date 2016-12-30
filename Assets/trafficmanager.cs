f4a9107c8b73eb2163b676faeb0ac00b64c360e4
ZakoSandi <zakomail10@gmail.com>
2016-12-30 20:47:49 +0100

    končana mapa
    nekaj novih stavb, dodani avomobili, se nepremikjoči, dodani pešci ki
    hodijo po pločniku in čez zebro, podrobnosti za konćnen izgled in
    funkcionalnost mape zadnji teden.

---
+++ Assets/trafficmanager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class trafficmanager : MonoBehaviour {
+
+    public GameObject lampObject;
+    public bool isGreen;
+    public Color greenColor, redColor;
+
+    private void Update()
+    {
+        lampObject.GetComponent<Material>().color = isGreen ? greenColor : redColor;
+    }
+}
